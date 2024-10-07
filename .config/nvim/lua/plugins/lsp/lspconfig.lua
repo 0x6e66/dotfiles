d00d6996c22c907af391715cc785086161db22a9
Frondorf, Niklas <niklas@frondorf.eu>
2024-10-07 19:53:46 +0200

    feat: added typst plugin

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/neodev.nvim", opts = {} },
+    { "folke/neodev.nvim",                   opts = {} },
   },
   config = function()
     local lspconfig = require("lspconfig")
@@ -93,7 +93,15 @@ return {
           },
         })
       end,
-    })
 
+      ["typst_lsp"] = function()
+        lspconfig["typst_lsp"].setup({
+          settings = {
+            exportPdf = "never",
+          },
+        })
+      end,
+
+    })
   end,
 }
