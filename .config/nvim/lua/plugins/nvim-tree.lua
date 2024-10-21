b604a407808cafb7afb98fde10ed8c47458c8c83
Frondorf, Niklas <niklas@frondorf.eu>
2024-10-21 14:00:33 +0200

    feat(nvim): removed .git directory from nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -5,6 +5,10 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+      filters = {
+        custom = { "^\\.git$" }
+      }
+    })
   end
 }
