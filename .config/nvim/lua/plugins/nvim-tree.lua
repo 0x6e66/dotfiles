f03a0eaf8b0fb8bb402d5c2fe93ba3dc42fc303f
Frondorf, Niklas <niklas@frondorf.eu>
2024-11-15 11:44:45 +0100

    Revert "feat(nvim): modified nvim-tree to show git-ignored files by default"
    This reverts commit d7a4087cb08ea19644348b9c3a2b21cff9b5709a.

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,17 +1,14 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-        require("nvim-tree").setup({
-            filters = {
-                custom = { "^\\.git$" }
-            },
-            git = {
-                ignore = false
-            }
-        })
-    end
+    require("nvim-tree").setup({
+      filters = {
+        custom = { "^\\.git$" }
+      }
+    })
+  end
 }
